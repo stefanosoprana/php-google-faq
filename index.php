b2efcbea552779afddf95f04eef8e53ef75234c6
stefanosoprana <stefano.soprana@icloud.com>
2019-01-23 17:11:52 +0100

    navbar

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>FAQ Google</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="logo">
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="">
+        <h1>Privacy & Terms</h1>
+      </div>
+      <div class="navbar">
+        <div class="navbar-left">
+          <ul>
+            <li>Overview</li>
+            <li>Privacy Policy</li>
+            <li>Term of Service</li>
+            <li>Technologies</li>
+            <li>FAQ</li>
+          </ul>
+        </div>
+        <div class="navbar-right">
+          Google Account
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
